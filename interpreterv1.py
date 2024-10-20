83e9a3d38447d98ebebf2b4c4a010cec27a9ccfa
ZCX031116 <Cixuan_Zhang@outlook.com>
2024-10-20 16:37:42 -0700

    Implement run_function & run_statement
    Implement run_function & run_statement

--- interpreterv1.py
+++ interpreterv1.py
@@ -24,19 +24,62 @@ class Interpreter(InterpreterBase):
             print("Received AST from parser.")
         self.vars_to_vals = {}
         main_found = False
-        if ast.elem_type == "program":
-            # Find finctions
-            if "functions" in ast.dict:
-                for func_elem in ast.dict["functions"]:
-                    # Get main function
-                    if func_elem.dict.get("name", None) == "main":
-                        self.do_function(func_elem)
-                        main_found = True
-                        break
+        num = len(ast.get("functions"))
+
+        if ast.elem_type == 'program':
+            for i in range(num):
+                if ast.get("functions")[i].get("name") == "main":
+                    main_found = True
+                    break
             if not main_found:
                 super().error(ErrorType.NAME_ERROR,
                           "No main() function was found",
                 )
+            for i in range(num):
+                if ast.get("functions")[i].get("name") == "main":
+                    self.run_function(ast.get("functions")[i])
+
+    def run_function(self, func_element):
+        if func_element.elem_type != "func" or "name" not in func_element.dict or  "statements" not in func_element.dict:
+            super().error(ErrorType.NAME_ERROR,"ERROR: Running run_function_ on invalid function element.")
+
+        if self.trace_output:
+            print(f'Running function: {func_element.dict["name"]}.')
+        for statement in func_element.dict["statements"]:
+            self.run_statement(statement)
+    
+    def run_statement(self, statement_elem):
+        if statement_elem.elem_type not in ["vardef","=", "fcall"] or "name" not in statement_elem.dict:
+            super().error(ErrorType.NAME_ERROR,"ERROR: Running run_statement on invalid statement element.")
+
+        if self.trace_output:
+            print(f'Running statement {statement_elem.elem_type} {statement_elem.dict["name"]}.')
+        if statement_elem.elem_type == "vardef":
+            if statement_elem.get("name") in self.vars_to_vals:
+                super().error(ErrorType.NAME_ERROR, f"Variable {statement_elem.get('name')} defined more than once",)
+            self.vars_to_vals[statement_elem.dict["name"]] = None
+
+        # Statement logic (assignment)
+        if statement_elem.elem_type == "=":
+            if "expression" not in statement_elem.dict:
+                super().error(ErrorType.NAME_ERROR,"ERROR: Statement element has no expression.")
+                exit()
+            if self.trace_output:
+                print("Statement is assignment.")
+            value = self.get_expr(statement_elem.get("expression"))
+            self.vars_to_vals[statement_elem.get("name")] = value
+
+        # Statment logic (fcall)
+        if statement_elem.elem_type == "fcall":
+            # Verify fcall structure
+            if "args" not in statement_elem.dict:
+                super().error(ErrorType.NAME_ERROR,"ERROR: Statement element has no args.")
+                exit()
+            # Trace output
+            if self.trace_output:
+                print("Statement is fcall.")
+            # Fcall logic
+            self.run_fcall(statement_elem)
 
 
 def main():
@@ -71,8 +114,8 @@ def main():
         print(a + b);
     }"""
 
-    new_interpreter = Interpreter(console_output = True, inp = None, trace_output = True)
+    new_interpreter = Interpreter(console_output = True, inp = None, trace_output = False)
     new_interpreter.run(test_program)
 
-# if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+   main()
